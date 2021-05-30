6d541aa09214e83821d26bdc7df37db211ddab48
Karina Doynova <kdoynova@gmail.com>
2021-05-30 17:43:59 +0300

    Update TablesEntitiesForeignPrimaryKey.sql

--- Exercises_EinstiegInSQL/TablesEntitiesForeignPrimaryKey.sql
+++ Exercises_EinstiegInSQL/TablesEntitiesForeignPrimaryKey.sql
@@ -72,4 +72,16 @@ VALUES
 select * from fachbuch;
 
 #Seite 334 Uebung 3
+INSERT INTO fachbereichbuch
+VALUES
+(3,1),
+(1,3),
+(4,4),
+(1,5),
+(1,6),
+(1,7),
+(2,8),
+(2,9);
+
+SELECT*FROM fachbereichbuch;
 
